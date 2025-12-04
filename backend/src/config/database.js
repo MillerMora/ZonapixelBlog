c830bb1f61bb69ed47118f543d720f196dbc7d3f
Miller Mora <millermora2022@gmail.com>
2025-12-04 15:46:08 -0500

    cambio de configuracion de base de datos con mssql

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,21 +1,41 @@
 const sql = require('mssql');
 const env = require('dotenv').config();
 
-const dbConfig = {
+const dbconfig = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    server: process.env.DB_HOST,
+    server: process.env.DB_SERVER,
     database: process.env.DB_NAME,
     options: {
-        encrypt: true, 
-        trustServerCertificate: true
+        encrypt: false,
+        trustservercertificate: true
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
     }
-};
+}
 
-const poolPromise = new sql.ConnectionPool(dbConfig) .connect().then(pool => {
-        console.log('Conexion exitosa a la base de datos');
+let pool;
+
+async function obtenerConexion() {
+
+    try {
+        if (pool) {
+            return pool;
+        }
+
+        pool = await sql.connect (dbconfig);
+        console.log("Conexion exitosa")
         return pool;
-    })
-    .catch(err => console.log('Conexion fallida ', err));
+    } catch (error) {
+        console.log("Error al conectarse a la base de datos")
+        throw error;
+    }
+    
+}
 
-    module.exports = { sql, poolPromise };
+module.exports = {
+    sql, obtenerConexion
+}
