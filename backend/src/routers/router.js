133bee763a558d1abc346038aeef7541a1c9434e
Miller Mora <millermora2022@gmail.com>
2025-12-04 19:56:23 -0500

    migracion de mssql a msql2 y actualizacion de readme

--- backend/src/routers/router.js
+++ backend/src/routers/router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Importar controladores
 const ArticuloController = require('../controllers/articuloController');
 const CategoriaController = require('../controllers/categoriaController');
 const ComentarioController = require('../controllers/comentarioController');
@@ -10,45 +9,37 @@ const OpinionController = require('../controllers/opinionController');
 const JuegoController = require('../controllers/juegoController');
 const ResenaController = require('../controllers/resenaController');
 
-// ========== RUTAS DE ARTÍCULOS ==========
 router.get('/articulos', ArticuloController.obtenerTodos);
 router.get('/articulos/recientes', ArticuloController.obtenerRecientes);
 router.get('/articulos/:id', ArticuloController.obtenerPorId);
 router.get('/articulos/categoria/:idCategoria', ArticuloController.obtenerPorCategoria);
 
-// ========== RUTAS DE CATEGORÍAS ==========
 router.get('/categorias', CategoriaController.obtenerTodos);
 router.get('/categorias/conteo', CategoriaController.obtenerConConteo);
 router.get('/categorias/:id', CategoriaController.obtenerPorId);
 
-// ========== RUTAS DE JUEGOS ==========
 router.get('/juegos', JuegoController.obtenerTodos);
 router.get('/juegos/recientes', JuegoController.obtenerRecientes);
 router.get('/juegos/:id', JuegoController.obtenerPorId);
 
-// ========== RUTAS DE RESEÑAS ==========
 router.get('/resenas', ResenaController.obtenerTodos);
 router.get('/resenas/mejor-calificadas', ResenaController.obtenerMejorCalificadas);
 router.get('/resenas/:id', ResenaController.obtenerPorId);
 router.get('/resenas/juego/:idJuego', ResenaController.obtenerPorJuego);
 
-// ========== RUTAS DE ENTREVISTAS ==========
 router.get('/entrevistas', EntrevistaController.obtenerTodos);
 router.get('/entrevistas/recientes', EntrevistaController.obtenerRecientes);
 router.get('/entrevistas/:id', EntrevistaController.obtenerPorId);
 
-// ========== RUTAS DE OPINIONES ==========
 router.get('/opiniones', OpinionController.obtenerTodos);
 router.get('/opiniones/populares', OpinionController.obtenerPopulares);
 router.get('/opiniones/:id', OpinionController.obtenerPorId);
 router.get('/opiniones/usuario/:idUsuario', OpinionController.obtenerPorUsuario);
 
-// ========== RUTAS DE COMENTARIOS ==========
 router.get('/comentarios/recientes', ComentarioController.obtenerRecientes);
 router.get('/comentarios/:id', ComentarioController.obtenerPorId);
 router.get('/comentarios/opinion/:idOpinion', ComentarioController.obtenerPorOpinion);
 
-// Ruta raíz de la API
 router.get('/', (req, res) => {
   res.json({
     message: 'API ZonaPixel - Bienvenido',
