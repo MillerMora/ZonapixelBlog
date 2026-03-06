30679c1741190484027f92fecd09857a9bcf7ba3
Miller Mora <millermora2022@gmail.com>
2026-03-05 20:15:27 -0500

    se añadio mas propiedades correspondientes a usuario

--- backend/ZonapixelApi/Models/User.cs
+++ backend/ZonapixelApi/Models/User.cs
@@ -7,11 +7,14 @@ namespace ZonapixelApi.Controllers.Models
 {
     public class User
     {
+
         public int Id { get; set; }
         public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
 
-        public string? PasswordConfirm { get; set;}
+        public string? ConfirmPassword { get; set;}
     }
 }
